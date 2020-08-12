be0d4f6c1c2cb169bca397caccd02f8ff56f8ad0
Nilton Pires <gabriel@moinhoglobo.com.br>
2020-08-12 13:54:39 -0300

    Changed cameraClient from function to class

--- cameraClient.js
+++ cameraClient.js
@@ -1,4 +1,30 @@
 const onvif = require('node-onvif')
+const connClient = require('./connClient')
+const fs = require('fs')
+
+class cameraClient extends connClient {
+
+  user = ''
+  pass = ''
+
+  constructor(host, user, pass) {
+    super(host, 0)
+    this.user = user
+    this.pass = pass
+  }
+
+  async snapshot() {
+    let device = new onvif.OnvifDevice({
+      xaddr: `http://${this.host}/onvif/device_service`,
+      user: this.user,
+      pass: this.pass
+    })
+
+    await device.init()
+    const res = await device.fetchSnapshot()
+    return res.body
+  }
+}
 
 //console.log('Start the discovery process.');
 // Find the ONVIF network cameras.
@@ -17,18 +43,20 @@ onvif.startProbe().then((device_info_list) => {
 });
 */
 
+/*
 async function snapshotCamera(host, user, pass) {
-    // Create an OnvifDevice object
-    let device = new onvif.OnvifDevice({
-        xaddr: `http://${host}/onvif/device_service`,
-        user,
-        pass
-    })
+  // Create an OnvifDevice object
+  let device = new onvif.OnvifDevice({
+    xaddr: `http://${host}/onvif/device_service`,
+    user,
+    pass
+  })
 
-    await device.init()
-    const res = await device.fetchSnapshot()
-    return res.body
+  await device.init()
+  const res = await device.fetchSnapshot()
+  return res.body
 }
+*/
 
 /*
 snapshotCamera('192.168.111.23', 'admin', 'admin').then(data => {
@@ -36,4 +64,12 @@ snapshotCamera('192.168.111.23', 'admin', 'admin').then(data => {
 })
 */
 
-module.exports = snapshotCamera
+const client = new cameraClient('192.168.111.23', 'admin', 'admin')
+/*
+client.snapshot().then(data => {
+  fs.writeFileSync('teste.jpg', data, { encoding: 'binary' })
+}).catch(e => {
+  console.log('error', e)
+})*/
+
+module.exports = cameraClient

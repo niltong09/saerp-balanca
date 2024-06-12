f0c0f984e6c680f60feb4107e36764054b6badf4
Nilton Pires <gabriel@MacBook-Pro-de-Nilton.local>
2024-06-12 12:14:35 -0300

    Fix: leitura com buffer para codigode barras

--- antClient.js
+++ antClient.js
@@ -1,7 +1,9 @@
 const connClient = require("./connClient");
+const net = require('net')
 
 class antClient extends connClient {
   processingTags = [];
+  bufferStr = ""
 
   convertDecHexWiegand(strdec) {
     if (strdec.length < 8) {
@@ -17,6 +19,18 @@ class antClient extends connClient {
     const self = this;
     self.addReadWatcher(async (tag) => {
       // console.log(`tag ${tag.toString().length}`);
+      if (self.port == 4001 || self.port == 4002) {
+        // leitor de cod barras usar buffer
+            if (tag.toString().substr(-1) != "\n" && tag.toString().substr(-1) != "\r") {
+              self.bufferStr += tag.toString()
+              return;
+            } else {
+              tag = self.bufferStr + tag.toString()
+              self.bufferStr = ""
+          }
+      }
+
+      }
       if (tag.toString().length > 3) {
         if (tag.length == 65) {
           // Tag de antena henry
@@ -91,30 +105,38 @@ ant.startAntMonitor(async (tag) => {
   console.log(`Readed ${tag}`);
 });
 */
-/*
+
 function connect(host, port, onReceiveTag) {
     return new Promise((resolve, reject) => {
         const client = new net.Socket()
+        let bufferstr = "";
 
         client.on('error', err => {
             reject(err)
         })
 
         client.on('data', data => {
-            if (data.toString().length < 5) {
+            console.log('data received', data, data.toString())
+            if (data.toString().substr(-1) != "\n" && data.toString().substr(-1) != "\r") {
+              bufferstr += data.toString()
+              return;
+            } else {
+              bufferstr += data.toString()
+      }
+            if (bufferstr.length < 5) {
                 return;
             }
-            console.log('data received', data)
             //console.log('data len', data.toString().length)
-            if (data.length == 65) {
+            if (bufferstr.length == 65) {
                 // Antena henry leitura decimal
                 // Dados: <79+REON+000+0]00000000000013658925]14/04/2020 10:07:07]1]0]2F
-                const splitedData = data.toString('ascii').split(']')
+                const splitedData = bufferstr.toString('ascii').split(']')
                 console.log('matricula', splitedData[1], (splitedData[1] * 1).toString(16))
                 onReceiveTag(splitedData[1])
             } else {
-                onReceiveTag(data.toString().replace("\r", "").replace("\n", ""))
+                onReceiveTag(bufferstr.toString().replace("\r", "").replace("\n", ""))
             }
+            bufferstr = "";
         })
 
         client.on('close', () => {
@@ -136,15 +158,15 @@ function sendComand(client, comand) {
     let checksum = [size, 0, ...eventbytes].reduce((a, p) => a ^ p)
     const dataSend = Buffer.from([0x02, size, 0, ...eventbytes, checksum, 0x03])
     return client.write(dataSend)
-}*/
+}
 
-/*
-const host = '192.168.111.12'
-const port = 8081
+
+const host = '192.168.111.10'
+const port = 4002
 connect(host, port, tag => {
     console.log('Readed tag', tag)
 
 }).then(client => {
     //sendComand(client, "00+RR+00+T]00000001]50")
-})*/
+})
 module.exports = antClient;
